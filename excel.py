a72431ecc3cf2954c2d8a1e0b7531e6668110fe5
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2023-02-26 18:28:15 +0530

    Refactor excel related functions into excel.py

---
+++ excel.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+def create_workbook():
+    from openpyxl import Workbook
+    from openpyxl.styles import Alignment, Border, Font, NamedStyle, PatternFill, Side
+
+    workbook = Workbook()
+    worksheet = workbook.active
+
+    worksheet.append(["Student ID", "Name", "Time"])
+    worksheet.freeze_panes = "A2"
+    worksheet.row_dimensions[1].height = 20
+    worksheet.column_dimensions["A"].width = 20
+    worksheet.column_dimensions["B"].width = 30
+    worksheet.column_dimensions["C"].width = 15
+
+    highlight = NamedStyle(name="highlight")
+    highlight.font = Font(name="Times New Roman", bold=True, size=14)
+    bd = Side(style="thin", color="000000")
+    highlight.border = Border(left=bd, top=bd, right=bd, bottom=bd)
+    highlight.fill = PatternFill("solid", fgColor="FFFF00")
+    highlight.alignment = Alignment(horizontal="center", vertical="center")
+
+    worksheet["A1"].style = highlight
+    worksheet["B1"].style = highlight
+    worksheet["C1"].style = highlight
+    return workbook
+
+
+def generate_attendance_report(attendees):
+    workbook = create_workbook()
+    worksheet = workbook.active
+
+    for roll_num in attendees:
+        worksheet.append([roll_num, attendees[roll_num][0], attendees[roll_num][1]])
+
+    workbook.save(f'attendance-records/{datetime.now().strftime("%d-%m-%y (%I.%M-%p)")}.xlsx')
